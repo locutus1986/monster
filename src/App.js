149987451a6c44095a6d51aed8af43667dfc505e
Zachary <locutus1986@gmail.com>
2022-05-12 14:24:13 -0400

    frist comit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { Component } from 'react'
+import "./App.css";
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount(){
+     fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res =>res.json())
+      .then(users => this.setState(() => 
+        {return {monsters: users}}
+      )
+    )
+  }
+
+  onSearchChange = e =>{ 
+    const searchField = e.target.value.toLowerCase()
+    this.setState(
+      () => { return { searchField }
+  })}
+
+  render(){
+
+    const {monsters, searchField } = this.state;
+    const { onSearchChange} = this;
+    const filteredMonsters = monsters.filter((monster)=> {
+      return monster.name.toLowerCase().includes(searchField)
+    })
+
+    return (
+      <div className="App">
+      <h1 class='app-title'>Welcone to the freak show!</h1>
+       
+       <SearchBox onChangeHandler={onSearchChange} placeholder="search monsters" className="search-box"/>
+        <CardList monsters={filteredMonsters} />
+        
+      </div>
+    );
+  }
+ 
+}
+
+export default App;
